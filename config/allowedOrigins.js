833002881786c758293f906480a1f2837402eef6
emtl688 <ericpmoran1@gmail.com>
2023-02-27 18:14:50 -0500

    changed allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://mycrm-ericm.onrender.com/'
 ]
 
 module.exports = allowedOrigins
