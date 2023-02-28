57a92c9f6438fe9be194134b375c20242ce99e61
emtl688 <ericpmoran1@gmail.com>
2023-02-28 00:35:11 -0500

    fix origin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3500;
 connectDB();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://mycrm-ericm.onrender.com/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
