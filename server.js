be689f46cf52f28e1dcb62544917023b16a7d506
emtl688 <ericpmoran1@gmail.com>
2023-02-27 19:31:10 -0500

    Access-Control-Allow-Headers

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Authorization, Content-Type")
   res.header('Content-Type', 'application/json');
   next();
 });
