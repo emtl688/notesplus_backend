e2b726d191e782442836c94e16336c7f643fd386
emtl688 <ericpmoran1@gmail.com>
2023-02-28 00:13:31 -0500

    try again

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://mycrm-ericm.onrender.com"); // set the origin of your app
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
