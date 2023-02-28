a395345f77629ed3b780420068a7a7771cf2fb45
emtl688 <ericpmoran1@gmail.com>
2023-02-28 00:31:12 -0500

    try this again

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const PORT = process.env.PORT || 3500;
 connectDB();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://mycrm-ericm.onrender.com"); // set the origin of your app
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
